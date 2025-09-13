3a5e3c6547ccd59e3f425be7c7ce622a0d5ce78d
Valentino <valentinolorca@gmail.com>
2025-09-13 17:35:31 -0300

    Subiendo archivo

---
+++ untitledLombok-DTO-1/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[]args){
+        Persona p1 = new Persona("Enzo Severino",19);
+        Persona p2 = new Persona();
+
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+}
---
+++ untitledLombok-DTO-1/src/main/java/Persona.java
@@ -0,0 +1,16 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Persona {
+    private String nombre;
+    private int edad;
+}
